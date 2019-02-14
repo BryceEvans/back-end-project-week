3ae2f2f19e278fecd01e9049da0069ddcd862ca0
BryceEvans <evans.bryce@gmail.com>
2019-02-13 22:46:17 -0700

    table created.

--- data/migrations/20190213211825_notes.js
+++ data/migrations/20190213211825_notes.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.scehma.createTable('notes', table => {
+    table.increments();
+    table.string('title').notNullable();
+    table.text('textBody').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('notes');
 };
