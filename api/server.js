28199648d588b2a6fbf5a21b5e4ae53fe0c77968
BryceEvans <evans.bryce@gmail.com>
2019-02-21 23:40:23 -0700

    * added to cors.

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const server = express();
 
 server.use(express.json());
-server.use(cors());
+server.use(cors('*'));
 
 server.get('/', (req, res) => {
   res.status(200).json({ server: 'up and running!'});
